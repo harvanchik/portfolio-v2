46f3b64137fad828cf59704e0d42136cf8b5ec00
harvanchik <66141166+harvanchik@users.noreply.github.com>
2022-02-21 20:54:33 -0800

    started index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   darkMode: 'class', // 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        handwriting: ['Gaegu', 'cursive'],
+      },
       screens: {
         xs: '500px',
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('tailwindcss-textshadow')],
 };
